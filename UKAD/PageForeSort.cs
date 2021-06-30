29e0af6d96ef5cfcd7e7c19fce209ca98f951938
ArtemiiNazarenko <artemnazarenk@gmail.com>
2021-06-30 15:45:42 +0300

    Добавьте файлы проекта.

---
+++ UKAD/PageForeSort.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UKAD
+{
+    class PageForeSort
+    {
+        public string link { get; set; }
+        public long time { get; set; }
+    }
+    class PageComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            return (new CaseInsensitiveComparer()).Compare(((PageForeSort)x).time, ((PageForeSort)y).time);
+        }
+    }
+}
